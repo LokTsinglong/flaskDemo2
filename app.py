ed4cfff85dd6262b0005233c67e2a80125a8b61e
Lok <1027408923@qq.com>
2025-04-27 14:55:09 +0800

    April 27 third save

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask import Flask
 from api.projects import projects_api
 from api.managers import managers_api
 from api.staff import staff_api
+from api.login import login_api
 
 from flask_migrate import Migrate
 from flask_cors import CORS
@@ -11,8 +12,13 @@ app=Flask(__name__,static_folder='img')
 app.register_blueprint(projects_api, url_prefix='/projects')
 app.register_blueprint(managers_api, url_prefix='/managers')
 app.register_blueprint(staff_api, url_prefix='/staff')
+app.register_blueprint(login_api, url_prefix='/login')
+app.secret_key = "my-secret-key-123" 
 
-CORS(app, resources={r"/x": {"origins": "x"}})
+CORS(app,
+    resources={r"/*": {"origins": "http://localhost:8080"}},  # 仅允许前端开发地址
+    supports_credentials=True  # 允许携带 Cookie
+    )
 
 class Config(object):
     SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:123456@localhost:3306/flask_test'
